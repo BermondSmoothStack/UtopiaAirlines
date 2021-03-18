95a3b2469c2b8a5bc46bc40870d1900f746947eb
Bermond Jon Batistiana <berjondev@gmail.com>
2021-03-17 22:22:16 -0700

    Route/RouteDAO

---
+++ src/main/java/apr/ss/utopia/dao/RouteDAO.java
@@ -0,0 +1,58 @@
+package apr.ss.utopia.dao;
+
+import apr.ss.utopia.entity.Airport;
+import apr.ss.utopia.entity.Route;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteDAO extends BaseDAO {
+
+    public RouteDAO(Connection conn) {
+        super(conn);
+    }
+
+    public void addRoute(Route route) throws SQLException {
+        save("insert into (" + Route.DEST_CODE + ", " + Route.ORIGIN_CODE + ") " + Route.NAME + " values (?)", new Object[]{route.getDestinationAirport(), route.getOriginAirport()});
+    }
+
+    public void updateRoute(Route route) throws SQLException {
+        save("update " + Route.NAME + " set " + Route.DEST_CODE + " = ?, " + Route.ORIGIN_CODE + " = ? where " + Route.ID + " = ?", new Object[]{route.getDestinationAirport(), route.getOriginAirport() ,route.getId()});
+    }
+
+    public void deleteRoute(Route route) throws SQLException {
+        save("delete from " + Route.NAME + " where " + Route.ID + " = ?", new Object[]{route.getId()});
+    }
+
+    public List<Route> readAllRoute() throws ClassNotFoundException, SQLException {
+        return read("select * from " + Route.NAME, null);
+    }
+
+    public List<Route> readRoutesByCode(Route route) throws ClassNotFoundException, SQLException {
+        return read("select * from " + Route.NAME + " where " + Route.ID + " = ", new Object[]{route.getId()});
+    }
+
+
+    @Override
+    public List<Route> extractData(ResultSet rs) throws SQLException {
+        List<Route> route = new ArrayList<>();
+        while (rs.next()) {
+            Route r = new Route();
+            Airport destination = new Airport();
+            Airport origin = new Airport();
+
+            destination.setAirportCode(rs.getString(Route.DEST_CODE));
+            origin.setAirportCode(rs.getString(Route.ORIGIN_CODE));
+
+            r.setId(rs.getInt(Route.ID));
+            r.setDestinationAirport(destination);
+            r.setOriginAirport(origin);
+
+            route.add(r);
+        }
+        return route;
+    }
+}
--- src/main/java/apr/ss/utopia/entity/Route.java
+++ src/main/java/apr/ss/utopia/entity/Route.java
@@ -1,4 +1,37 @@
 package apr.ss.utopia.entity;
 
 public class Route {
+
+    public static final String NAME = "route";
+    public static final String ID = "id";
+    public static final String ORIGIN_CODE = "origin_id";
+    public static final String DEST_CODE = "destination_id";
+
+    private Integer id;
+    private Airport destinationAirport;
+    private Airport originAirport;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Airport getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(Airport destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public Airport getOriginAirport() {
+        return originAirport;
+    }
+
+    public void setOriginAirport(Airport originAirport) {
+        this.originAirport = originAirport;
+    }
 }
