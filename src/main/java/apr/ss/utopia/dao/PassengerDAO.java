1162406b882f700ae4a9b09d11089755fbdd3120
Bermond Jon Batistiana <berjondev@gmail.com>
2021-03-18 13:51:14 -0700

    Passenger DAO/Entity

---
+++ src/main/java/apr/ss/utopia/dao/PassengerDAO.java
@@ -0,0 +1,87 @@
+package apr.ss.utopia.dao;
+
+import apr.ss.utopia.entity.Booking;
+import apr.ss.utopia.entity.Passenger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PassengerDAO extends BaseDAO<Passenger> {
+
+    public PassengerDAO(Connection conn) {
+        super(conn);
+    }
+
+    public void addPassenger(Passenger passenger) throws SQLException {
+        save("insert into (" +
+                        Passenger.BOOKING + ", " +
+                        Passenger.GVN_NAME + ", " +
+                        Passenger.FAM_NAME + ", " +
+                        Passenger.DOB + ", " +
+                        Passenger.GENDER + ", " +
+                        Passenger.ADDR + ", " +
+                        ") " + Passenger.NAME + " values (?, ?, ?, ?, ?, ?)",
+                new Object[]{
+                        passenger.getBooking().getId(),
+                        passenger.getGivenName(),
+                        passenger.getFamilyName(),
+                        java.sql.Date.valueOf(passenger.getDob().toLocalDate()),
+                        passenger.getGender(),
+                        passenger.getAddress()
+                });
+    }
+
+    public void updatePassenger(Passenger passenger) throws SQLException {
+        save("update " + Passenger.NAME + " set " +
+                        Passenger.BOOKING + " = ?, " +
+                        Passenger.GVN_NAME + " = ?, " +
+                        Passenger.FAM_NAME + " = ?, " +
+                        Passenger.DOB + " = ?, " +
+                        Passenger.GENDER + " = ?, " +
+                        Passenger.ADDR + " = ? " +
+                        "where " + Passenger.ID + " = ?",
+                new Object[]{
+                        passenger.getBooking().getId(),
+                        passenger.getGivenName(),
+                        passenger.getFamilyName(),
+                        java.sql.Date.valueOf(passenger.getDob().toLocalDate()),
+                        passenger.getGender(),
+                        passenger.getAddress()
+                });
+    }
+
+    public void deletePassenger(Passenger passenger) throws SQLException {
+        save("delete from " + Passenger.NAME + " where " + Passenger.ID + " = ?", new Object[]{passenger.getId()});
+    }
+
+    public List<Passenger> readAllPassenger() throws ClassNotFoundException, SQLException {
+        return read("select * from " + Passenger.NAME, null);
+    }
+
+    public List<Passenger> readPassengersByCode(Passenger passenger) throws ClassNotFoundException, SQLException {
+        return read("select * from " + Passenger.NAME + " where " + Passenger.ID + " = ?", new Object[]{passenger.getId()});
+    }
+
+    @Override
+    public List<Passenger> extractData(ResultSet rs) throws SQLException {
+        List<Passenger> passengers = new ArrayList<>();
+        while (rs.next()) {
+            Passenger p = new Passenger();
+            Booking b = new Booking();
+
+            b.setId(rs.getInt(Passenger.BOOKING));
+
+            p.setBooking(b);
+            p.setId(rs.getInt(Passenger.ID));
+            p.setGivenName(rs.getString(Passenger.GVN_NAME));
+            p.setFamilyName(rs.getString(Passenger.FAM_NAME));
+            p.setDob(rs.getTimestamp(Passenger.DOB).toLocalDateTime());
+            p.setGender(rs.getString(Passenger.GENDER));
+            p.setAddress(rs.getString(Passenger.ADDR));
+        }
+        return passengers;
+    }
+}
---
+++ src/main/java/apr/ss/utopia/entity/Passenger.java
@@ -0,0 +1,94 @@
+package apr.ss.utopia.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Passenger {
+
+    public static final String NAME = "passenger";
+    public static final String ID = "id";
+    public static final String BOOKING = "booking_id";
+    public static final String GVN_NAME = "given_name";
+    public static final String FAM_NAME = "family_name";
+    public static final String DOB = "dob";
+    public static final String GENDER = "gender";
+    public static final String ADDR = "address";
+
+    private Integer id;
+    private Booking booking;
+    private String givenName;
+    private String familyName;
+    private LocalDateTime dob;
+    private String gender;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passenger passenger = (Passenger) o;
+        return id.equals(passenger.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
